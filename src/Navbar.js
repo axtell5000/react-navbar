a3fef0b5f3179362351f52c1f5233c804e110f19
Stephen Axtell <axtell.stephen@gmail.com>
2020-11-12 12:30:23 +0200

    making toggle more dynamic using useRef

--- src/Navbar.js
+++ src/Navbar.js
@@ -7,18 +7,34 @@ const Navbar = () => {
   // state
   const [showLinks, setShowLinks] = useState(false);
 
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+  const toggleLinks = () => {
+    setShowLinks(!showLinks);
+  };
+
+  // useEffect run when showLinks value changes, where we calc the heights
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (showLinks) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = '0px';
+    }
+  }, [showLinks]);
+
   return (
     <nav>
       <div className="nav-center">
         <div className="nav-header">
           <img src={logo} className='logo' alt='logo' />
-          <button className='nav-toggle' onClick={() => setShowLinks(!showLinks)}>
+          <button className='nav-toggle' onClick={toggleLinks}>
             <FaBars />
           </button>
         </div>
         
-        <div className={`${ showLinks ? 'links-container show-container' : 'links-container'}`}>
-          <ul className="links"> 
+        <div className="links-container" ref={linksContainerRef}>
+          <ul className="links" ref={linksRef}>
             {links.map(link => {
               const { id, url, text } = link;
               return (
